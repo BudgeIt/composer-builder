3f7eebee8ec2352575a25535293b8264c0454157
John P Bloch <johnpbloch@gmail.com>
2015-10-25 15:27:03 -0500

    Make the plugin an event subscriber

--- src/Plugin.php
+++ src/Plugin.php
@@ -3,10 +3,13 @@
 namespace BudgeIt\ComposerBuilder;
 
 use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
+use Composer\Script\Event;
+use Composer\Script\ScriptEvents;
 
-class Plugin implements PluginInterface
+class Plugin implements PluginInterface, EventSubscriberInterface
 {
 
     /**
@@ -17,7 +20,41 @@ class Plugin implements PluginInterface
      */
     public function activate(Composer $composer, IOInterface $io)
     {
-        // TODO: Implement activate() method.
+        // Nothing to do here
+    }
+
+    /**
+     * Returns an array of event names this subscriber wants to listen to.
+     *
+     * The array keys are event names and the value can be:
+     *
+     * * The method name to call (priority defaults to 0)
+     * * An array composed of the method name to call and the priority
+     * * An array of arrays composed of the method names to call and respective
+     *   priorities, or 0 if unset
+     *
+     * For instance:
+     *
+     * * array('eventName' => 'methodName')
+     * * array('eventName' => array('methodName', $priority))
+     * * array('eventName' => array(array('methodName1', $priority), array('methodName2'))
+     *
+     * @return array The event names to listen to
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            ScriptEvents::POST_INSTALL_CMD => [['runInstallers', 10], ['runBuildTools', 5]],
+            ScriptEvents::POST_UPDATE_CMD => [['runInstallers', 10], ['runBuildTools', 5]],
+        ];
+    }
+
+    public function runInstallers(Event $event)
+    {
+    }
+
+    public function runBuildTools(Event $event)
+    {
     }
 
 }
